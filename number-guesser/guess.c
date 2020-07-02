32cf78ce8ab60fe7bbc98c8d8764bb0e7f6209d5
bheki-maenetja <bhekimaenetja@gmail.com>
2020-07-02 11:12:29 +0200

    implemented upper and lower bounds

--- number-guesser/guess.c
+++ number-guesser/guess.c
@@ -3,12 +3,22 @@
 
 int main(void)
 {
-  int upper_bound, lower_bound;
-  upper_bound = 9;
-  lower_bound = 4;
-  for (int i = 0; i < 5; i++)
+  int upper_bound, lower_bound, user_num;
+  printf("Enter a lower bound: ");
+  scanf("%d", &lower_bound);
+  printf("Enter an upper bound: ");
+  scanf("%d", &upper_bound);
+  
+  do
   {
-    int rand_num = (rand() % (upper_bound - lower_bound + 1)) + lower_bound;
-    printf("%d\n", rand_num);
-  }
+    printf("Enter a number between %d and %d: ", lower_bound, upper_bound);
+    scanf("%d", &user_num);
+  } while (user_num > upper_bound || user_num < lower_bound);
+  
+  printf("You have chosen %d\n", user_num);
+  // for (int i = 0; i < 5; i++)
+  // {
+  //   int rand_num = (rand() % (upper_bound - lower_bound + 1)) + lower_bound;
+  //   printf("%d\n", rand_num);
+  // }
 }
