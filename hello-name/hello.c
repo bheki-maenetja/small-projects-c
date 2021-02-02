408911528acc43f205f6ae5abc7cba5cd483bb72
bheki-maenetja <bhekimaenetja@gmail.com>
2021-02-02 14:10:09 +0000

    learning c all over again

--- hello-name/hello.c
+++ hello-name/hello.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-  char my_name[100];
-  printf("Enter your name: ");
-  scanf("%s", my_name);
-  printf("Hello, %s\n", my_name);
+    char my_name[100];
+    printf("Please enter your name: ");
+    scanf("%s", my_name);
+    printf("Hello, %s\n", my_name);
 }
