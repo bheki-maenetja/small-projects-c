0e408ba1362bcde0990751985f1da8e95fc5dc20
bheki-maenetja <bhekimaenetja@gmail.com>
2021-02-03 13:16:16 +0000

    refactored input function

--- hello-name/hello.c
+++ hello-name/hello.c
@@ -4,6 +4,7 @@ int main(void)
 {
     char my_name[100];
     printf("Please enter your name: ");
-    scanf("%s", my_name);
+    // scanf("%s", my_name);
+    fgets(my_name, 100, stdin);
     printf("Hello, %s\n", my_name);
 }
